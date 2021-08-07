e850ed75561808866455db77f884cd43996ab781
shamu <kssoe99@gmail.com>
2021-08-07 16:49:04 +0630

    Added setting Allow host

--- ERP/settings.py
+++ ERP/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b50@c_pa%qz$#y@-m1-b(dvsar@j9*g1((42m16a0#hly^+jl6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1','mydjangoerpapp.herokuapp.com']
 
 
 # Application definition
